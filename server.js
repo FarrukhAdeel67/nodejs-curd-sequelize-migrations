87a0febc084123d8e8834046648053009f7260f3
Farrukh Adeel <81711287+FarrukhAdeel67@users.noreply.github.com>
2022-08-21 03:24:29 -0700

    Create server.js

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const moment = require("moment");
+const port = 4000;
+const app = express();
+require("dotenv").config();
+const studentRouter = require('./routes/student');
+app.use(express.json());
+
+
+app.use("/api/students", studentRouter);
+
+app.listen(port,()=>{
+    console.log(`Server running at http://localhost:${port}`);
+})

74e7b8db7fac1e53f76c6b5c4f2b773a936291d5
Farrukh Adeel <81711287+FarrukhAdeel67@users.noreply.github.com>
2022-08-31 00:31:06 -0700

    Update server.js

--- server.js
+++ server.js
@@ -1,14 +1,13 @@
-const express = require('express');
+const express = require("express");
 const moment = require("moment");
 const port = 4000;
 const app = express();
 require("dotenv").config();
-const studentRouter = require('./routes/student');
+const studentRouter = require("./routes/student");
 app.use(express.json());
-
 
 app.use("/api/students", studentRouter);
 
-app.listen(port,()=>{
-    console.log(`Server running at http://localhost:${port}`);
-})
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
